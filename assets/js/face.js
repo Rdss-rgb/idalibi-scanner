66117de6125d0a55ebda5dcb3d727329682237d2
Drenz <drendarjosh@gmail.com>
2024-04-06 14:44:10 +0800

    ppp

--- assets/js/face.js
+++ assets/js/face.js
@@ -197,7 +197,7 @@ function draw(canvasOutputCtx, canvasInputCtx) {
 
 async function postJSON(data) {
     try {
-      const response = await fetch("https://64bc-112-199-114-2.ngrok-free.app/api/login", {
+      const response = await fetch("https://78a6-112-199-114-2.ngrok-free.app/api/login", {
         method: "POST", // or 'PUT'
         headers: {
           "Content-Type": "application/json",
